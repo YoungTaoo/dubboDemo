7cd7335e9af82538fded5b22246b8549ecc5d3eb
YoungTaoo <18905883998@163.com>
2017-11-07 14:17:03 +0800

    第一次idea和github的结合使用

---
+++ dubbo-provider/src/main/java/com/alibaba/dubbo/demo/entity/UserEntity.java
@@ -0,0 +1,32 @@
+package com.alibaba.dubbo.demo.entity;
+
+public class UserEntity {
+
+    private String id;
+    private int age;
+    private int sex;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+}
